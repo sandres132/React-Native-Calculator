fa35a6071e3f2522a452070a1151de3de169cf06
andres <60414032+sandres132@users.noreply.github.com>
2025-01-24 13:01:59 -0600

    Estructura para el proyecto agregada

---
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function CalculatorScreen() {
+    return (
+        <View>
+            <Text>CalculatorScreen</Text>
+        </View>
+    )
+}
